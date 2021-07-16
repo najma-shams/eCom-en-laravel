341c0783336686109f5af1fbbf979655be6a0f9c
najma-shams <najmashams4@gmail.com>
2021-07-16 11:15:45 -0700

    login done

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,9 +1,4 @@
-{{-- <div class="panel panel-default">
-<div class="panel-body">
-Panel Content
-</div>
-<div class="panel-footer">Panel Footer</div>
-</div> --}}
+
 
 
 <footer class="text-center text-white" style="background-color: #f1f1f1;">
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,17 +5,18 @@
 <div class="container custom-login">
 <div class="row">
 <div class="col-sm-4 mx-auto">
-    <form>
+    <form action="login" method="POST">
     <div class="form-group">
+     @csrf
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" name="" id="exampleInputEmail1" placeholder="Email">
+    <input type="email" name="email" class="form-control" name="" id="exampleInputEmail1" placeholder="Email">
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" name="" id="exampleInputPassword1" placeholder="Password">
+        <input type="password" name="password" class="form-control" name="" id="exampleInputPassword1" placeholder="Password">
     </div>
 
-    <button type="submit" class="btn btn-default">Submit</button>
+    <button type="submit" class="btn btn-default">Login</button>
     </form>
 </div>
 </div>
